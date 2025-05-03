46f167f473cc56bd8c2657a7454724611c899711
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-05-03 13:01:32 +0530

    add list

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const TodoItem = () => {
+const TodoItem = ({ todo }) => {
+
+  const [completed, setCompleted] = useState(todo.completed);  
+
+  const toggle = () => {
+    setCompleted(!completed);  
+  };
+
   return (
     <div>
-      
+      <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={toggle}>
+        {completed ? 'Undo' : 'Complete'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
